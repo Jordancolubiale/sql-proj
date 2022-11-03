4201da802efdaa4331e0df713eafca1f26f543ff
Jordan Colubiale <jordancolubiale@Jordans-Air-2.attlocal.net>
2022-11-03 17:25:59 -0400

    added db.query for view functions fixed seeds.sql and changed topic of interest

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,15 +1,34 @@
-INSERT INTO department (department_id, department_name)
-VALUES (1, "Home Decor"),
-       (2, "Technology"),
-       (3, "Bakery"),
-       (4, "Sports"),
-       (5, "Seafood");
+INSERT INTO department (name)
+VALUES ("Boxer"),
+       ( "Trainer"),
+       ( "Family"),
+       ( "Friend");
 
-INSERT INTO role (id, title)
-VALUES (1, "Customer Service"),
-       (2, "Stocker"),
-       (3, "Inventory"),
-       (4, "Cashier"),
-       (5, "Manager"),
-       (6, "Bagger"),
-       (7, "Janitor");
+INSERT INTO role (title, salary, department_id)
+VALUES ( "Protagonist", 60000, 1),
+       ( "Antagonist", 40000, 1),
+       ( "Family", 20000, 3 ),
+       ( "Training Crew", 15000, 2),
+       ( "Extras", 5000, 4);
+
+       INSERT INTO employee(first_name, last_name, role_id) VALUES ("Rocky", "Balboa", 1);
+       INSERT INTO employee(first_name, last_name, role_id) VALUES ("Adrian", "Balboa", 3);
+       INSERT INTO employee(first_name, last_name, role_id) VALUES ("Paulie", "Pennino", 3);
+       INSERT INTO employee(first_name, last_name, role_id) VALUES ("Apollo", "Creed", 2);
+       INSERT INTO employee(first_name, last_name, role_id) VALUES ("Mickey", "Goldmill", 4);
+       INSERT INTO employee(first_name, last_name, role_id) VALUES ("Clubber", "Lang", 2);
+       INSERT INTO employee(first_name, last_name, role_id) VALUES ("Ivan", "Drago", 2);
+       INSERT INTO employee(first_name, last_name, role_id) VALUES ("Rocky", "Balboa Jr.", 3);
+       INSERT INTO employee(first_name, last_name, role_id) VALUES ("Adonis", "Creed", 1);
+       INSERT INTO employee(first_name, last_name, role_id) VALUES ("Tommy", "Gunn", 2);
+       INSERT INTO employee(first_name, last_name, role_id) VALUES ("Mason", "Dixon", 2);
+       INSERT INTO employee(first_name, last_name, role_id) VALUES ("Bianca", "Taylor", 5);
+       INSERT INTO employee(first_name, last_name, role_id) VALUES ("Ludmilla", "Drago", 2);
+       INSERT INTO employee(first_name, last_name, role_id) VALUES ("Mary Anne", "Creed", 5);
+       INSERT INTO employee(first_name, last_name, role_id) VALUES ("Tony", "Gazzo", 5);
+       INSERT INTO employee(first_name, last_name, role_id) VALUES ("Cuff", "Balboa", 3);
+       INSERT INTO employee(first_name, last_name, role_id) VALUES ("Link", "Balboa", 3);
+       INSERT INTO employee(first_name, last_name, role_id) VALUES ("Butkus", "Balboa", 3);
+       INSERT INTO employee(first_name, last_name, role_id) VALUES ("Tony", "Evers", 4);
+       INSERT INTO employee(first_name, last_name, role_id) VALUES ("George", "Jergens", 5);
+       INSERT INTO employee(first_name, last_name, role_id) VALUES ("Father", "Carmine", 5);
--- server.js
+++ server.js
@@ -1,107 +1,123 @@
-const express = require('express');
 // Import and require mysql2
 const mysql = require('mysql2');
 
-const inquirer = require ('inquirer');
-
-const PORT = process.env.PORT || 3001;
-const app = express();
-
-// Express middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-
+const inquirer = require('inquirer');
 // Connect to database
 const db = mysql.createConnection(
-  {
-    host: 'localhost',
-    // MySQL username,
-    user: 'root',
-    // TODO: Add MySQL password here
-    password: 'rootroot',
-    database: 'workplace_db'
-  },
-  console.log(`Connected to the workplace_db database.`)
+    {
+        host: 'localhost',
+        // MySQL username,
+        user: 'root',
+        // TODO: Add MySQL password here
+        password: 'rootroot',
+        database: 'workplace_db'
+    },
+    console.log(`Connected to the workplace_db database.`)
 );
 
 const questions = [{
-    type: 'text',
-    message: 'Github username?',
-    name: 'github',
-},
-{
-    type: 'text',
-    message: 'Email used for github?',
-    name: 'email',
-},
-{
-    type: 'text',
-    message: 'List steps to install',
-    name: 'install',
-},
-{
-    type: 'text',
-    message: 'List any third party assets used',
-    name: 'thirdParty',
-},
-{
-    type: 'text',
-    message: 'Project Name?',
-    name: 'title',
-},
-{
-    type: 'text',
-    message: 'Short Project Description.',
-    name: 'description',
-},
-{
-    type: 'text',
-    message: 'List your fellow Collaborators',
-    name: 'team',
-},
-{
-    type: 'input',
-    message: 'What should be done to run tests?',
-    name: 'test',
-},
-{
-    type: 'input',
-    message: 'What does the user need to know about using the repo?',
-    name: 'usage',
-},
-{
-    type: 'input',
-    message: 'Who else helped contribute to the project?',
-    name: 'contributions',
-},
-
-{
     type: 'list',
-    name: 'license',
-    message: 'What kind of license should your project have?',
-    choices: ['MIT', 'GNU', ],
-    default: ["MIT"],
+    name: 'startup',
+    message: 'What do you want to do?',
+    choices: ["view all departments", "view all roles", "view all employees", "add a department", "add a role", "add an employee", "update an employee role"],
     validate: nameInput => {
         if (nameInput) {
             return true;
         } else {
-            console.log('Please choose a license!');
+            console.log('Please choose!');
             return false;
         }
     }
-},
+}]
 
-{
-    type: 'text',
-    message: 'What is your Github URL',
-    name: 'gitHub',
-},
-];
+// {
+//     type: 'text',
+//     message: 'Email used for github?',
+//     name: 'email',
+// },
+// {
+//     type: 'text',
+//     message: 'List steps to install',
+//     name: 'install',
+// },
+// {
+//     type: 'text',
+//     message: 'List any third party assets used',
+//     name: 'thirdParty',
+// },
+// {
+//     type: 'text',
+//     message: 'Project Name?',
+//     name: 'title',
+// },
+// {
+//     type: 'text',
+//     message: 'Short Project Description.',
+//     name: 'description',
+// },
+// {
+//     type: 'text',
+//     message: 'List your fellow Collaborators',
+//     name: 'team',
+// },
+// {
+//     type: 'input',
+//     message: 'What should be done to run tests?',
+//     name: 'test',
+// },
+// {
+//     type: 'input',
+//     message: 'What does the user need to know about using the repo?',
+//     name: 'usage',
+// },
+// {
+//     type: 'input',
+//     message: 'Who else helped contribute to the project?',
+//     name: 'contributions',
+// },
 
+// {
+//     type: 'text',
+//     message: 'What is your Github URL',
+//     name: 'gitHub',
+// },
+// ];
+function view_departments() {
+    db.query("SELECT * FROM department", function (err, result) {
+        if (err) throw err
+        console.table(result)
+        init()
+    })
+}
+function view_roles() {
+    db.query("SELECT * FROM role", function (err, result) {
+        if (err) throw err
+        console.table(result)
+        init()
+    })
+}
+function view_employees() {
+    db.query("SELECT * FROM employee", function (err, result) {
+        if (err) throw err
+        console.table(result)
+        init()
+    })
+}
 function init() {
     inquirer
         .prompt(questions)
-        .then(responses => {writeToFile('README.md' ,generateMarkdown({...responses}));
-});}
+        .then(responses => {
+            console.log(responses)
+            if (responses.startup === "view all departments") {
+                view_departments()
+            }
+            if (responses.startup === "view all roles") {
+                view_roles()
+            }
+            if (responses.startup === "view all employees") {
+                view_employees()
+            }
+        });
+}
 // Function call to initialize app
 init();
